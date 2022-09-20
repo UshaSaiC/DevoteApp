6e8615546cf7bf20a6848023cf8a9a65986c3dbb
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-21 04:53:46 +0530

    Added custom header

--- DevoteApp/View/NewTaskItemView.swift
+++ DevoteApp/View/NewTaskItemView.swift
@@ -12,6 +12,7 @@ struct NewTaskItemView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @State private var task: String = ""
     @Binding var isShowing: Bool
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     
     @FocusState private var keyboardIsFocused: Bool
     
@@ -48,7 +49,7 @@ struct NewTaskItemView: View {
                     .focused($keyboardIsFocused)
                     .padding()
                     .background(
-                        Color(UIColor.systemGray6)
+                        isDarkMode ? Color(UIColor.tertiarySystemBackground) : Color(UIColor.secondarySystemBackground)
                     )
                     .cornerRadius(10)
                 
@@ -71,7 +72,7 @@ struct NewTaskItemView: View {
             .padding(.horizontal)
             .padding(.vertical, 20)
             .background(
-                Color.white
+                isDarkMode ? Color(UIColor.secondarySystemBackground) : Color.white
             )
             .cornerRadius(16)
             .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
